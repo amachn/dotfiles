b74be200655e478c713f2e49d36b6a0009b122e9
amachn <amachann2005@gmail.com>
2024-12-20 03:08:28 -0600

    add: symlink scripts

---
+++ git/link.ps1
@@ -0,0 +1,2 @@
+New-Item -Path ~\.gitconfig -ItemType SymbolicLink -Value D:\dotfiles\git\.gitconfig
+New-Item -Path D:\tools\Git\etc\gitconfig -ItemType SymbolicLink -Value D:\dotfiles\git\gitconfig
---
+++ vscode/link.ps1
@@ -0,0 +1,1 @@
+New-Item -Path ~\AppData\Roaming\Code\User\settings.json -ItemType SymbolicLink -Value D:\dotfiles\vscode\settings.json
